b7eed0d8381e0c434ed285c3b7627aaf6ae56461
Zoha-Arif <Zoha-Arif@users.noreply.github.com>
2018-12-08 19:02:32 -0500

    lll

--- main.js
+++ main.js
@@ -11,12 +11,43 @@ function typeWriter() {
   }
 }
 
-var input = $('#play').val();
 $(document).ready(function(){ //when document is ready, function will be executed.
     $(document).keypress(function(key){
-        if (key.which == 13 || key.which == '13'){
+        if (key.which == 13){
+            var input = $('#play').val();
             if (input == "yes" || input == "y"){
                 window.location('game.html'); 
+            } 
+            else if (input == "no" || input == "n"){ 
+                var i = 0;
+                var txt = "Aw, you're missing out on an adventure! "; /* The text */
+                var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+                function typeWriter() {
+                    if (i < txt.length) {
+                      document.getElementById("no").innerHTML += txt.charAt(i);
+                      i++;
+                      setTimeout(typeWriter, speed);
+                    }
+                  }
+
+                  typeWriter.call(); 
+            } 
+
+            else {
+                var i = 0;
+                var txt = "Uh oh, I don't recognize that answer! Please answer yes or no. "; /* The text */
+                var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+                function typeWriter() {
+                    if (i < txt.length) {
+                      document.getElementById("no").innerHTML += txt.charAt(i);
+                      i++;
+                      setTimeout(typeWriter, speed);
+                    }
+                  }
+
+                  typeWriter.call();
             }
         }        
     })
